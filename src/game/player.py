3a706f3a1a461b1a5fe53e9c6596790029287fbb
Isaac <imorsan@correo.ugr.es>
2026-04-30 16:27:07 +0100

    Fix MCTS_Node initialization and exploration parameter

--- src/game/player.py
+++ src/game/player.py
@@ -93,7 +93,7 @@ class RandomPlayer(Player):
 class MCTS_Node:
 
     #  is the C value in MCTS
-    def __init__(self, board, player_id, move=None, parent=None, =math.sqrt(2)):
+    def __init__(self, board, player_id, move=None, parent=None, exploration=math.sqrt(2)):
 
         self.board = board
         self.player_id = player_id
@@ -104,7 +104,7 @@ class MCTS_Node:
         self.visits = 0
         self.children = []  # Expanded children list
         self.untried_moves = board.get_possible_moves(player_id)
-        self. = 
+        self.exploration = exploration 
 
     def is_fully_expanded(self):
         return len(self.untried_moves) == 0
@@ -137,7 +137,7 @@ class MCTS_Node:
         best_score = -1
 
         for child in self.children:
-            score = child.uct_score(self.)
+            score = child.uct_score(self.exploration)
 
             if best_node is None or score > best_score:
                 best_score = score
