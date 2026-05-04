d933fda7b6b1e5401621b9aefc411c1e44a71d86
Deviloria <daniviloria03@gmail.com>
2026-05-04 13:36:09 +0100

    Fix lint: remove unused pytest import

--- tests/test_dataset_generator.py
+++ tests/test_dataset_generator.py
@@ -3,7 +3,6 @@
 # =================================
 
 import os
-import pytest
 from src.game.board import PLAYER1, PLAYER2
 from src.game.player import RandomPlayer
 from src.game.dataset_generator import simulate_game, generate_dataset
